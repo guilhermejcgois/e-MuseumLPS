f92c7ac6e8f1bf9b36d74e571246fd4d5560594e
Guilherme Jose de Carvalho Gois <guijocargo@gmail.com>
2016-06-02 10:12:39 -0300

    Only required files, more work on features files and added Apache Commons CLI for product's interface.

---
+++ features/Museum/com/lpsmuseum/Main.java
@@ -0,0 +1,116 @@
+package com.lpsmuseum;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.UnrecognizedOptionException;
+
+public class Main {
+	
+	private static Map<String, Boolean> features = new HashMap<String, Boolean>();
+	private static Options options = new Options();
+	
+	private static void verifyFeatures() {
+		try {
+			Class.forName("com.lpsmuseum.entity.MuseologicalObjectDO");
+			features.put("object", true);
+		} catch (ClassNotFoundException exception) {
+			features.put("object", false);
+		}
+		try {
+			Class.forName("com.lpsmuseum.entity.ImageDO");
+			features.put("image", true);
+		} catch (ClassNotFoundException exception) {
+			features.put("image", false);
+		}
+		try {
+			Class.forName("com.lpsmuseum.entity.TextDO");
+			features.put("text", true);
+		} catch (ClassNotFoundException exception) {
+			features.put("text", false);
+		}
+	}
+	
+	private static void createOptions() {
+		// options.addOption(cmd, hasArgs, decription);
+		
+		if (features.get("object")) {
+			ActionOption object = new ActionOption("object", true, "Do an action with 'object' feature.");
+			object.add("create");
+			object.add("list");
+			options.addOption(object);
+		}
+		/*options.addOption(ActionOption.builder("object")
+				.hasArg().argName("create").argName("list")
+				.desc("Do an action with 'object' feature.")
+				.build());*/
+		options.addOption(ActionOption.builder("exit")
+				.desc("Exit the program.")
+				.build());
+	}
+	
+	public static void main(String[] args) {
+		boolean goAhead = true;
+		
+		verifyFeatures();
+		createOptions();
+		
+		CommandLineParser parser = new DefaultParser();
+		Scanner scanner = new Scanner(System.in);
+		
+		while (goAhead) {
+			try {
+				System.out.print(">> ");
+				String cmds = scanner.nextLine();
+				
+				if (cmds.isEmpty())
+					continue;
+				
+				cmds = "-".concat(cmds);
+				CommandLine line = parser.parse(options, cmds.split(" "));
+				
+				String action = null;
+				if (line == null) {
+					continue;
+				} else if (line.hasOption("object")) {
+					action = line.getOptionValue("object");
+					ActionOption option = (ActionOption) options.getOption("object");
+					
+					if (!option.isValid(action))
+						throw new ParseException("Invalid action for object feature");
+					
+					
+				} else if (line.hasOption("exit")) {
+					goAhead = false;
+				}
+			} catch (ParseException exception) {
+				System.out.println();
+				System.out.println("<feature> <action>");
+				for (Option option : options.getOptions()) {
+					System.out.print("    " + option.getOpt());
+					if (option instanceof ActionOption) {
+						Object actions[] = ((ActionOption) option).getActionArray();
+						if (actions.length > 0)
+							System.out.print("\t" + actions[0]);
+						for (int i = 1; i < actions.length; i++)
+							System.out.print("|" + actions[i]);
+					}
+					System.out.println("\t" + option.getDescription());
+				}
+				System.out.println();
+			}
+		}
+		
+		System.out.println("Good bye!!");
+	}
+
+}
